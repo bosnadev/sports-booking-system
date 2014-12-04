e7b5904853e00c2f2e2fd447881063af3860039b
emchooo <emir.softic@gmail.com>
2014-12-04 21:47:50 +0100

    added create and edit user

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -1,6 +1,29 @@
-@foreach($users as $user)
+@extends('layout.main')
 
-    {{ $user }}
-    <br/>
+@section('content')
 
-@endforeach
+<table class="table table-hover">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th> Actions </th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($users as $user)
+        <tr>
+            <td>{{ $user->id }}</td>
+            <td> {{ $user->first_name }}</td>
+            <td> {{ $user->last_name }}</td>
+            <td> {{ $user->email }}</td>
+            <td> {{ link_to_route('users.edit' , 'Edit' , $user->id) }} </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+
+
+@stop
